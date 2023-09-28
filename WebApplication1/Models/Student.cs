0940eb84879613154dba285e828230c0dbe47a25
hai nguyen <haiknt2002@gmail.com>
2023-09-28 13:38:34 +0700

    update Annotation

--- WebApplication1/Models/Student.cs
+++ WebApplication1/Models/Student.cs
@@ -5,14 +5,19 @@ namespace BTH1.Models
     public class Student
     {
         public int Id { get; set; }//Mã sinh viên
-        [Required]
+
+        [Required(ErrorMessage = "Tên là trường bắt buộc.")]
+        [StringLength(100, MinimumLength = 4, ErrorMessage = "Tên phải có ít nhất 4 ký tự và tối đa 100 ký tự.")]
         public string? Name { get; set; } //Họ tên
-        [Required(ErrorMessage = "Email bắt buộc phải được nhập")]
-        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}")]
+        [Required(ErrorMessage = "Email là trường bắt buộc.")]
+        [RegularExpression(@"^[\w-]+(\.[\w-]+)*@gmail\.com$", ErrorMessage = "Địa chỉ email phải có đuôi gmail.com.")]
         public string? Email { get; set; } //Email
         [StringLength(100, MinimumLength = 8)]
-        [Required]
+        
+        [Required(ErrorMessage = "Mật khẩu là trường bắt buộc.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W).{8,}$", ErrorMessage = "Mật khẩu phải từ 8 ký tự trở lên và phải chứa ít nhất một chữ thường, một chữ viết hoa, một số và một ký tự đặc biệt.")]
         public string? Password { get; set; }//Mật khẩu
+
         [Required]
         public Branch? Branch { get; set; }//Ngành học
         [Required]
@@ -26,6 +31,9 @@ namespace BTH1.Models
         [Required]
         public DateTime DateOfBorth { get; set; }//Ngày sinh
 
+        [Required(ErrorMessage = "Điểm là trường bắt buộc.")]
+        [Range(0.0, 10.0, ErrorMessage = "Điểm phải nằm trong khoảng từ 0.0 đến 10.0.")]
+        public float Score { get; set; }
 
     }
 }
